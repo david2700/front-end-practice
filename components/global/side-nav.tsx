254dd14b4bcba3d1724b8cb74ae2b0269ecde0f7
David <ademide6@gmail.com>
2025-04-15 09:27:47 +0100

    Add SideNav component to layout and update Home page with welcome message

---
+++ components/global/side-nav.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const SideNav = () => {
+  return (
+    <div className='md:flex flex-col dispplay-none md:w-[200px] min-h-screen h-full bg-amber-200'>SideNav</div>
+  )
+}
+
+export default SideNav;
